0e5923e2a2e9b2979c853fc08db762fcd0e28985
Darko Ilievski <100212395+cllinkz@users.noreply.github.com>
2022-05-13 14:23:51 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 'use strict';
 
 // Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
 
@@ -833,20 +831,43 @@ Some_Variable
   calculate_AGE
 delayed_departure
 */
-document.body.append(document.createElement('textarea'));
+// document.body.append(document.createElement('textarea'));
 
-document.body.append(document.createElement('button'));
+// document.body.append(document.createElement('button'));
 
-document.querySelector('button').addEventListener('click', function () {
-  const text = document.querySelector('textarea').value;
-  const rows = text.split('\n');
+// document.querySelector('button').addEventListener('click', function () {
+//   const text = document.querySelector('textarea').value;
+//   const rows = text.split('\n');
 
-  for (const [i, row] of rows.entries()) {
-    const [first, second] = row.toLowerCase().trim().split('_');
-    const output = `${first}${second.replace(
-      second[0],
-      second[0].toUpperCase()
-    )}`;
-    console.log(`${output.padEnd(20)}${'%'.repeat(i + 1)}`);
-  }
-});
+//   for (const [i, row] of rows.entries()) {
+//     const [first, second] = row.toLowerCase().trim().split('_');
+//     const output = `${first}${second.replace(
+//       second[0],
+//       second[0].toUpperCase()
+//     )}`;
+//     console.log(`${output.padEnd(20)}${'%'.repeat(i + 1)}`);
+//   }
+// });
+
+// Coding challenge #5
+
+// Function used in the code
+const getCode = str => str.slice(0, 3).toUpperCase();
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+// SPLIT THE STRING IN ARRAY
+// console.log(flights.split('+'));
+// CREATE A FOR OF LOOP TO LOG THE STRINGS AS SEPARATE LINES
+for (const flight of flights.split('+')) {
+  // SPLIT THE SINGLE LINE OF STRING INTO AN ARRAY + DESTRUCTURING []
+  const [type, from, to, time] = flight.split(';');
+  // OUTPUT STRING, PLUS FORMAT THE [type, form, to, time] INDIVIDUALLY
+  const output = `${type.startsWith('_Delayed') ? '!!!' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} from ${getCode(from)} to ${getCode(to)} (${time.replace(
+    ':',
+    'h'
+  )})`.padStart(45, '-');
+  console.log(output);
+}
